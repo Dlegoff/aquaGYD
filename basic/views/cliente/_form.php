abbf45a21df7225fec21ce74d17009d7ae12b49b
yamilsosa <40873907+yamilsosa@users.noreply.github.com>
2018-12-03 19:45:12 -0300

    Clientes
    avance 1

--- basic/views/cliente/_form.php
+++ basic/views/cliente/_form.php
@@ -11,21 +11,59 @@ use yii\widgets\ActiveForm;
 <div class="cliente-form">
 
     <?php $form = ActiveForm::begin(); ?>
-
-    <?= $form->field($model, 'NroCli')->textInput() ?>
-
-    <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'observaciones')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'idCuenta')->textInput() ?>
-
-    <?= $form->field($model, 'idLoc')->textInput() ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+<div class="container-fluid">
+        <div class="panel panel-default">       
+            <div class="panel-body flex-vertical-center">
+                <div class="col-sm-6 padding-0 tt-enc-obj">
+                    Nuevo Cliente
+                </div>
+                <div class="col-sm-6 padding-0" align="right">
+                    <?=Html::a( 'Volver ',['index'], [ 'class' => 'btn btn-buscar', 'title' => utf8_encode('Volver')] ); ?>
+                </div>
+            </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="container-fluid flex-vertical-center">
+                    <div class="col-sm-2 padding-0">Nº Cliente:</div>
+                    <div class="col-sm-10 padding-0">
+                        <?= $form->field($model, 'NroCli')->textInput()->label(false) ?>
+                    </div>
+                </div>
+                <div class="container-fluid flex-vertical-center">
+                    <div class="col-sm-2 padding-0">Nombre:</div>
+                    <div class="col-sm-10 padding-0">
+                        <?= $form->field($model, 'nombre')->textInput(['maxlength' => true])->label(false) ?>
+                    </div>
+                </div>
+                <div class="container-fluid flex-vertical-center">
+                    <div class="col-sm-2 padding-0">Observaciones:</div>
+                    <div class="col-sm-10 padding-0">
+                        <?= $form->field($model, 'observaciones')->textInput(['maxlength' => true])->label(false) ?>
+                    </div>
+                </div>
+                <div class="container-fluid flex-vertical-center">
+                    <div class="col-sm-2 padding-0">Nº Cuenta:</div>
+                    <div class="col-sm-10 padding-0">
+                        <?= $form->field($model, 'idCuenta')->textInput()->label(false) ?>
+                    </div>
+                </div>
+                <div class="container-fluid flex-vertical-center">
+                    <div class="col-sm-2 padding-0">Localidad:</div>
+                    <div class="col-sm-10 padding-0">
+                        <?= $form->field($model, 'idLoc')->textInput()->label(false) ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="form-group" style="text-align:right">
+                <?= Html::submitButton('Grabar', ['class' => 'btn btn-success']) ?>
+            </div>
+        </div>
     </div>
-
+</div>
     <?php ActiveForm::end(); ?>
 
 </div>
--- basic/views/cliente/index.php
+++ basic/views/cliente/index.php
@@ -12,26 +12,46 @@ $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="cliente-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <div class="container-fluid">
+        <div class="panel panel-default">       
+            <div class="panel-body flex-vertical-center">
+                <div class="col-sm-8 padding-0 tt-enc-obj">
+                    <?= Html::encode($this->title) ?>
+        <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                </div>
+                <div class="col-sm-4 padding-0" align='right'>
+                        <?php 
+                            echo Html::a('Nuevo', ['create'], ['class' => 'btn btn-success']);
 
-    <p>
-        <?= Html::a('Create Cliente', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'NroCli',
-            'nombre',
-            'observaciones',
-            'idCuenta',
-            'idLoc',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
+                            echo Html::a( 'Volver ',['index'], [ 'class' => 'btn btn-buscar', 'title' => utf8_encode('Volver')] );
+                        ?>
+                    </div>
+             </div>
+        </div>
+        <div class="panel panel-default">
+            <div class="panel-heading panel-header-gral">
+                <h3 class="panel-title"> Datos Generales:</h3>
+            </div>
+            <div class="panel-body">
+                <div class="container-fluid" style="margin-top:10px;">
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'headerRowOptions' => ['class' => 'grilla'],
+                        'summaryOptions' => ['class' => 'hidden'],
+                        'rowOptions' => ['class' => 'grilla'],
+                        'columns' => [
+                            //['class' => 'yii\grid\SerialColumn'],
+                            ['attribute' => 'NroCli', 'label' => 'Nº Cliente','contentOptions' => [ 'style' => 'text-align:center'] ],
+                            ['attribute' => 'nombre', 'label' => 'Nombre','contentOptions' => [ 'style' => 'text-align:center'] ],
+                            ['attribute' => 'observaciones', 'label' => 'Observaciones','contentOptions' => [ 'style' => 'text-align:center'] ],
+                            ['attribute' => 'idCuenta', 'label' => 'Nº Cuenta','contentOptions' => [ 'style' => 'text-align:center'] ],
+                            ['attribute' => 'idLoc', 'label' => 'Cod. Localidad','contentOptions' => [ 'style' => 'text-align:center'] ],
+                            ['class' => 'yii\grid\ActionColumn'],
+                        ],
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
