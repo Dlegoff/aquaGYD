04c9f557cbc8baf518b1f110906c8146c9dc1a3d
yamilsosa <40873907+yamilsosa@users.noreply.github.com>
2018-12-08 14:29:23 -0300

    Login

--- basic/views/site/login.php
+++ basic/views/site/login.php
@@ -10,38 +10,59 @@ use yii\bootstrap\ActiveForm;
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <?php $form = ActiveForm::begin([
-        'id' => 'login-form',
-        'layout' => 'horizontal',
-        'fieldConfig' => [
-            'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-            'labelOptions' => ['class' => 'col-lg-1 control-label'],
-        ],
-    ]); ?>
-
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-        <?= $form->field($model, 'password')->passwordInput() ?>
-
-        <?= $form->field($model, 'rememberMe')->checkbox([
-            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
-        ]) ?>
-
-        <div class="form-group">
-            <div class="col-lg-offset-1 col-lg-11">
-                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+<?php $form = ActiveForm::begin([
+    'id' => 'login-form',
+    'layout' => 'horizontal',
+    'fieldConfig' => [
+        'template' => "{label}\n<div class=\"col-sm-3\">{input}</div>\n<div class=\"col-sm-3\">{error}</div>",
+        'labelOptions' => ['class' => 'col-sm-1 control-label'],
+    ],
+]); ?>
+<div class="container-fluid">
+    <div class="panel panel-titulo-login">
+        <div class="panel-group tt-enc-login"> Iniciar Sesión </div>
+    </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="container-fluid">
+                <div class="form-group">
+                    <div class="col-sm-12 col-sm-offset-4" align="center">
+                        <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Usuario') ?>
+                    </div>
+                </div>
             </div>
+            <div class="container-fluid">
+                <div class="form-group">
+                    <div class="col-sm-12 col-sm-offset-4" align="center">
+                        <?= $form->field($model, 'password')->passwordInput()->label('Contraseña') ?>
+                        <?= $form->field($model, 'rememberMe')->checkbox([
+                            'template' => "<div class=\"col-lg-offset-1 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-8\">{error}</div>",
+                        ]) ?>
+                    </div>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <div class="form-group">
+                    <div class="col-sm-12" align="center">
+                        <?= Html::submitButton('Login', ['class' => 'btn btn-buscar', 'name' => 'login-button']) ?>
+                    </div>
+                </div>
+            </div>
+         <div class="alert alert-info tt-enc-12">
+                <span class="glyphicon glyphicon-info-sign"></span><b> Al loguearse se podran consultar las siguientes secciones:</b>
+                <ul>
+                    <li><b>Listado de Usuarios:</b> Consultar los Datos de los Usuarios que tienen acceso al Sistema.</li>
+                    <li><b>Listado de Descuentos:</b> Consultar los Datos de los Descuentos disponibles.</li>
+                    <li><b>Para modificar usuario y cotraseña ir a:</b> <code>app\models\User::$users</code>.</li>
+                </ul>
+            </div>  
         </div>
-
-    <?php ActiveForm::end(); ?>
-
-    <div class="col-lg-offset-1" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
     </div>
 </div>
+<?php 
+
+echo Html::errorSummary( $model, ['id' => 'login-form','style' => 'margin-top: 10px']);
+ActiveForm::end(); ?>
+
+
+
