739b5dc80501c0cba2aca6dfd038beeba63eab9b
yamilsosa <40873907+yamilsosa@users.noreply.github.com>
2018-12-10 16:35:36 -0300

    search

--- basic/views/cliente/_search.php
+++ basic/views/cliente/_search.php
@@ -9,25 +9,38 @@ use yii\widgets\ActiveForm;
 ?>
 
 <div class="cliente-search">
+<div class="container-fluid">
 
     <?php $form = ActiveForm::begin([
         'action' => ['index'],
         'method' => 'get',
     ]); ?>
-
-    <?= $form->field($model, 'NroCli') ?>
-
-    <?= $form->field($model, 'nombre') ?>
-
-    <?= $form->field($model, 'observaciones') ?>
-
-    <?= $form->field($model, 'idCuenta') ?>
-
-    <div class="form-group">
-        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
-        <?= Html::resetButton('Reset', ['class' => 'btn btn-default']) ?>
+    
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <div class="container-fluid flex-vertical-center">
+                <div class="col-sm-2 padding-0">Nº Cliente:</div>
+                <div class="col-sm-1 padding-0">
+                    <?= $form->field($model, 'NroCli')->label(false) ?>
+                </div>
+                <div class="col-sm-2 padding-0">Nombre:</div>
+                <div class="col-sm-1 padding-0">
+                    <?= $form->field($model, 'nombre')->label(false) ?>
+                </div>
+                <div class="col-sm-2 padding-0">Nº Cuenta:</div>
+                <div class="col-sm-1 padding-0">
+                    <?= $form->field($model, 'idCuenta')->label(false) ?>
+                </div>
+                <div class="col-sm-3 padding-0" align="right">
+                    <?= Html::submitButton('Buscar', ['class' => 'btn btn-buscar']) ?>
+                    <?= Html::resetButton('Borrar', ['class' => 'btn btn-default']) ?>
+                </div>
+            </div>
+            
+        </div>
     </div>
 
     <?php ActiveForm::end(); ?>
 
+</div>
 </div>
