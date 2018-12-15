636ed9f9a15cdac1de8b06596d0b691b2499985f
yamilsosa <40873907+yamilsosa@users.noreply.github.com>
2018-12-14 21:44:45 -0300

    menu

---
+++ basic/models/Consultas.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+//use app\utils\db\utb;
+
+
+class Consultas extends \yii\db\ActiveRecord
+{
+	
+	public $nombre;
+	public $cuit;
+
+
+	/*public function rules()
+    {
+        return [
+         
+		];
+    }*/
+
+   
+}
